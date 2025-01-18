a9d65093ed5b0db390ad09e7a5c6068be227ecfe
mnrdsnts <meinardsantoss@gmail.com>
2025-01-18 18:58:45 +0800

    ➕ Added website logo

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata = {
   title: "UMAK Chat",
   description: "A platform where students can engage in conversation with other university students",
+  icons: {
+    icon: "/umak_logo.png",
+  }
 };
 
 export default function RootLayout({ children }) {
